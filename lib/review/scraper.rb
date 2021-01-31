1021fd3cd9d552d903b17778fcd16fd230e4bef7
Lucas Koontz <lucas.emanuel19@gmail.com>
2021-01-31 15:37:27 -0300

    add: gemspec setup

---
+++ lib/review/scraper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "scraper/version"
+
+module Review
+  module Scraper
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/review/scraper_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Review::Scraper do
+  it "has a version number" do
+    expect(Review::Scraper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
